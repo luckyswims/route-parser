9741d6c98ea2b5dd3e711ed2c4c3fd74a34fab0e
John Lucker <john.lucker@comcast.net>
2020-09-04 12:21:58 -0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import './App.css'
+import axios from 'axios'
 
 function App() {
+  const [route, setRoute] = useState(["Loading"])
+  const [isLoaded, setIsLoaded] = useState(false)
+  const [step, setStep] = useState(0)
+  const loadSheet = () => {
+    axios.get("https://sheets.googleapis.com/v4/spreadsheets/1MHWsGUOfXQ6qYEkbgGzc6svhlt9pr0ZlLd9nWUjtr7Q/values/Sheet1!A:A?key=AIzaSyByszzO7OEIsYazzKUA52ngaNKTEIspeDs")
+    .then(res => {
+      console.log(res)
+      setRoute(res.data.values)
+      setIsLoaded(true)
+    })
+  }
+  useEffect(() => { loadSheet() }, [])
+  let display
+  if (isLoaded) {
+    display = (
+      <div class="display">
+        <img onClick={() => setStep(step - 1)} src="./src/left-arrow.png" alt="Previous Step"/>
+        <p>{route[step]}</p>
+        <img onClick={() => setStep(step + 1)} src="" alt="Next Step"/>
+      </div>
+    )
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Route Parser</h1>
       </header>
+      <main>
+        {display}
+      </main>
+      <div>Icons made by <a href="https://www.flaticon.com/authors/roundicons" title="Roundicons">Roundicons</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
     </div>
   );
 }
